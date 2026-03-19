5c2c69d5111d3da3037bbab31ffcf2ddcbf7a055
Hadis Amin <hadisssurya@gmail.com>
2026-03-19 10:28:49 +0000

    update parameter

--- config/train_rocstories.py
+++ config/train_rocstories.py
@@ -3,8 +3,8 @@
 # $ py train.py config/train_rocstories.py
 
 out_dir = 'out-rocstories'
-# init_from = 'resume'  
-always_save_checkpoint = True  # save every checkpoint for quick runs
+init_from = 'resume'  
+always_save_checkpoint = False  # save every checkpoint for quick runs
 
 wandb_log = True # set to True to log training and validation metrics to Weights and Biases (wandb)
 wandb_project = 'rocstories'
@@ -16,28 +16,28 @@ dataset = 'rocstories'
 
 batch_size = 16     # larger stable training, smaller for saving memory GPU
 # Parameters that affect the model size
-block_size = 256   # larger more understanding context, smaller faster runs
-n_layer = 16        # larger for better reasoning(complexity understanding)
-n_head = 8         # larger for more various attention patterns(better prespectives) 
-n_embd = 256        # larger for more representation
+block_size = 256    # larger more understanding context, smaller faster runs
+n_layer = 6         # larger for better reasoning(complexity understanding)
+n_head = 6          # larger for more various attention patterns(better prespectives) 
+n_embd = 384        # larger for more representation
 
 # Fine-tuning parameters
 
 eval_interval = 200                 # higher for faster runs, smaller for better monitoring
-eval_iters = 40                     # higher for stable loss validation
-log_interval = 20                   # higher more frequent logging
+eval_iters = 100                    # higher for stable loss validation
+log_interval = 50                   # higher more frequent logging
 gradient_accumulation_steps = 8     # higher for stable training, smaller for faster runs
 
-dropout = 0.15                       # larger for less overfitting, smaller for overfitting
-learning_rate = 1e-3                # larger for faster training, smaller for better accuracy
-max_iters = 6000                    # larger for longer training(might increase accuracy), smaller for faster runs
-lr_decay_iters = 4500   # starting step for learning rate decay
-min_lr = 5e-4                       # learning rate after decay
-warmup_iters = 500                  # larger for more stable training - might spike at the start 
-weight_decay = 0.15                 # larger for more regularization(less overfitting), smaller for less
+dropout = 0.2                     # larger for less overfitting, smaller for overfitting
+learning_rate = 9e-6                # larger for faster training, smaller for better accuracy
+max_iters = 10000                    # larger for longer training(might increase accuracy), smaller for faster runs
+lr_decay_iters = 10000               # starting step for learning rate decay
+min_lr = 5e-6                       # learning rate after decay
+warmup_iters = 1000                 # larger for more stable training - might spike at the start 
+weight_decay = 0.2                  # larger for more regularization(less overfitting), smaller for less
 beta1 = 0.9                         # larger for more stable training, smaller for faster convergence
 beta2 = 0.95                        # larger for more stable training, smaller for faster convergence
-grad_clip = 1                     # larger for more stable training(gradient might be exploding), smaller for faster convergence
+grad_clip = 1                       # larger for more stable training(gradient might be exploding), smaller for faster convergence
 
 bias = False        
 device = 'cuda'
