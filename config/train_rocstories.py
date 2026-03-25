99601a2ed20773a09ebecf398de63f3c2a5766ad
Hadis Amin <hadisssurya@gmail.com>
2026-03-25 01:01:29 +0000

    update everything, ready to train after getting the parameter from tutor

--- config/train_rocstories.py
+++ config/train_rocstories.py
@@ -4,7 +4,7 @@
 
 out_dir = 'out-rocstories'
 # init_from = 'resume'  
-always_save_checkpoint = False  # save every checkpoint for quick runs
+always_save_checkpoint = True  # save every checkpoint for quick runs
 
 wandb_log = True # set to True to log training and validation metrics to Weights and Biases (wandb)
 wandb_project = 'rocstories'
@@ -14,31 +14,32 @@ dataset = 'rocstories'
 
 # Main architectural parameters
 
-batch_size = 8     # larger stable training, smaller for saving memory GPU
+batch_size = 64     # larger stable training, smaller for saving memory GPU
 # Parameters that affect the model size
-block_size = 512    # larger more understanding context, smaller faster runs
-n_layer = 128         # larger for better reasoning(complexity understanding)
-n_head = 64         # larger for more various attention patterns(better prespectives) 
-n_embd = 128        # larger for more representation
+block_size = 128    # larger more understanding context, smaller faster runs
+n_layer = 6         # larger for better reasoning(complexity understanding)
+n_head = 6         # larger for more various attention patterns(better prespectives) 
+n_embd = 384        # larger for more representation
 
 # Fine-tuning parameters
 
-eval_interval = 400                 # higher for faster runs, smaller for better monitoring
-eval_iters = 200                    # higher for stable loss validation
-log_interval = 50                   # higher more frequent logging
-gradient_accumulation_steps = 8     # higher for stable training, smaller for faster runs
+# eval_interval = 400                 # higher for faster runs, smaller for better monitoring
+# eval_iters = 200                    # higher for stable loss validation
+# log_interval = 50                   # higher more frequent logging
+# gradient_accumulation_steps = 8     # higher for stable training, smaller for faster runs
+dtype = float16 # 
 
-dropout = 0.1                     # larger for less overfitting, smaller for overfitting
-learning_rate = 6e-4                # larger for faster training, smaller for better accuracy
-max_iters = 6000                    # larger for longer training(might increase accuracy), smaller for faster runs
-lr_decay_iters = 6000               # starting step for learning rate decay
-min_lr = 6e-5                      # learning rate after decay
-warmup_iters = 200                 # larger for more stable training - might spike at the start 
-weight_decay = 0.1                 # larger for more regularization(less overfitting), smaller for less
-beta1 = 0.9                         # larger for more stable training, smaller for faster convergence
-beta2 = 0.95                        # larger for more stable training, smaller for faster convergence
-grad_clip = 1                       # larger for more stable training(gradient might be exploding), smaller for faster convergence
+dropout = 0.2                     # larger for less overfitting, smaller for overfitting
+learning_rate = 1e-3               # larger for faster training, smaller for better accuracy
+max_iters = 5000                    # larger for longer training(might increase accuracy), smaller for faster runs
+# lr_decay_iters = 6000               # starting step for learning rate decay
+# min_lr = 1e-4                      # learning rate after decay
+warmup_iters = 500                 # larger for more stable training - might spike at the start 
+# weight_decay = 0.2                 # larger for more regularization(less overfitting), smaller for less
+# beta1 = 0.9                         # larger for more stable training, smaller for faster convergence
+beta2 = 0.99                        # larger for more stable training, smaller for faster convergence
+# grad_clip = 1                       # larger for more stable training(gradient might be exploding), smaller for faster convergence
 
-bias = False        
+# bias = False        
 device = 'cuda'
-compile = False
+compile = True
